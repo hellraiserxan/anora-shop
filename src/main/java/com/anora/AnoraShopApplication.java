fc645e97ca27f27334f700984f248c04a284bf4f
Марк Мкртчян <isaqwa787@gmail.com>
2025-05-18 20:28:53 +0700

    init проекта

---
+++ src/main/java/com/anora/AnoraShopApplication.java
@@ -0,0 +1,13 @@
+package com.anora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnoraShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnoraShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anora/AnoraShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnoraShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
