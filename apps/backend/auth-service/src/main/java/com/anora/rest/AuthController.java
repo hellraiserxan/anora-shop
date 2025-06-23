3e85e0a528ab4580efdd1f2fed2a27958716a5ae
Марк Мкртчян <isaqwa787@gmail.com>
2025-06-23 20:52:33 +0700

    был добавлен AuthController

---
+++ apps/backend/auth-service/src/main/java/com/anora/rest/AuthController.java
@@ -0,0 +1,21 @@
+package com.anora.rest;
+
+import com.anora.models.User;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@AllArgsConstructor
+public class AuthController {
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user){
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/register")
+    public String paint(){
+        return "yes";
+    }
+}
